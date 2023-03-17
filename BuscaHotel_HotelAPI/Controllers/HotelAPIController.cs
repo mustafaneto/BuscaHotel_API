7a780cf01371429fc1cb9d5303301635bf9c5f39
Mustafa Neto <mustafaneto26@gmail.com>
2023-03-17 13:52:03 -0300

    Standard API Response

--- BuscaHotel_HotelAPI/Controllers/HotelAPIController.cs
+++ BuscaHotel_HotelAPI/Controllers/HotelAPIController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Net;
 using System.Reflection.Metadata.Ecma335;
 
 namespace BuscaHotel_HotelAPI.Controllers
@@ -19,6 +20,7 @@ namespace BuscaHotel_HotelAPI.Controllers
     {
         private readonly ILogging _logger;
 
+        protected APIResponse _response;
         private readonly IHotelRepository _dbHotel;
         private readonly IMapper _mapper;
 
@@ -27,38 +29,66 @@ namespace BuscaHotel_HotelAPI.Controllers
             _dbHotel = dbHotel;
             _logger = logger;
             _mapper = mapper;
+            this._response = new();
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
 
-        public async Task<ActionResult <IEnumerable<HotelDTO>>> GetHoteis()
+        public async Task<ActionResult<APIResponse>> GetHoteis()
         {
+            try
+            {
                 _logger.Log("Exibindo todos os hoteis", "");
                 IEnumerable<Hotel> hotelList = await _dbHotel.GetAllAsync();
-                return Ok (_mapper.Map<List<HotelDTO>>(hotelList));
+                _response.Result = _mapper.Map<List<HotelDTO>>(hotelList);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
+            return _response;
+
         }
 
-        [HttpGet("{id:int}", Name = "CreateHotel")]
+        [HttpGet("{id:int}", Name = "GetHotel")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
-        public async Task<ActionResult <HotelDTO>> GetHotel(int id)
+        public async Task<ActionResult<APIResponse>> GetHotel(int id)
         {
-            if (id == 0)
+            try
             {
-                _logger.Log("Tente um Id diferente de " + id, "error" );
-                return BadRequest();
-            }
+                if (id == 0)
+                {
+                    _logger.Log("Tente um Id diferente de " + id, "error");
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
 
-            var hotel = await _dbHotel.GetAsync(u => u.Id == id);
-            if (hotel == null)
+                var hotel = await _dbHotel.GetAsync(u => u.Id == id);
+                if (hotel == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+                _response.Result = _mapper.Map<HotelDTO>(hotel);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
             {
-                return NotFound();
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
             }
+            return _response;
 
-            return Ok(_mapper.Map<HotelDTO>(hotel));
         }
 
         [HttpPost]
@@ -66,25 +96,35 @@ namespace BuscaHotel_HotelAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
-        public async Task<ActionResult<HotelDTO>> CreateHotel([FromBody]HotelCreateDTO createDTO) 
+        public async Task<ActionResult<APIResponse>> CreateHotel([FromBody] HotelCreateDTO createDTO)
         {
-
-            if(await _dbHotel.GetAsync(u=>u.Nome.ToLower()== createDTO.Nome.ToLower())!=null)
+            try
             {
-                ModelState.AddModelError("CustomError", "Hotel já existe!");
-                return BadRequest(ModelState);
+                if (await _dbHotel.GetAsync(u => u.Nome.ToLower() == createDTO.Nome.ToLower()) != null)
+                {
+                    ModelState.AddModelError("CustomError", "Hotel já existe!");
+                    return BadRequest(ModelState);
+                }
+                if (createDTO == null)
+                {
+                    return BadRequest(createDTO);
+                }
+
+                Hotel hotel = _mapper.Map<Hotel>(createDTO);
+
+                await _dbHotel.CreateAsync(hotel);
+
+                _response.Result = _mapper.Map<HotelDTO>(hotel);
+                _response.StatusCode = HttpStatusCode.Created;
+                return CreatedAtRoute("GetHotel", new { id = hotel.Id }, _response);
             }
-            if (createDTO == null)
+            catch (Exception ex)
             {
-                return BadRequest(createDTO);
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
             }
-
-            Hotel model = _mapper.Map<Hotel>(createDTO);
-
-            await _dbHotel.CreateAsync(model);
-
-            return CreatedAtRoute("CreateHotel", new { id = model.Id } , model);
-
+            return _response;
         }
 
         [ProducesResponseType(StatusCodes.Status204NoContent)]
@@ -92,19 +132,31 @@ namespace BuscaHotel_HotelAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpDelete("{id:int}", Name = "DeleteHotel")]
 
-        public async Task<IActionResult> DeleteHotel(int id)
+        public async Task<ActionResult<APIResponse>> DeleteHotel(int id)
         {
-            if(id == 0)
+            try
             {
-                return BadRequest();
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+                var hotel = await _dbHotel.GetAsync(u => u.Id == id);
+                if (hotel == null)
+                {
+                    return NotFound();
+                }
+                await _dbHotel.RemoveAsync(hotel);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
             }
-            var hotel = await _dbHotel.GetAsync(u => u.Id == id);
-            if (hotel == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
             }
-            await _dbHotel.RemoveAsync(hotel);
-            return NoContent();
+            return _response;
         }
 
 
@@ -112,18 +164,30 @@ namespace BuscaHotel_HotelAPI.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
 
-        public async Task<IActionResult> UpdateHotel(int id, [FromBody] HotelUpdateDTO updateDTO)
+        public async Task<ActionResult<APIResponse>> UpdateHotel(int id, [FromBody] HotelUpdateDTO updateDTO)
         {
-            if (updateDTO == null || id != updateDTO.Id)
+            try
             {
-                return BadRequest();
+                if (updateDTO == null || id != updateDTO.Id)
+                {
+                    return BadRequest();
+                }
+
+                Hotel model = _mapper.Map<Hotel>(updateDTO);
+
+                await _dbHotel.UpdateAsync(model);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
             }
-
-            Hotel model = _mapper.Map<Hotel>(updateDTO);
-
-            await _dbHotel.UpdateAsync(model);
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
 
         [HttpPatch("{id:int}", Name = "UpdatePartialHotel")]
@@ -136,7 +200,7 @@ namespace BuscaHotel_HotelAPI.Controllers
             {
                 return BadRequest();
             }
-            var hotel = await _dbHotel.GetAsync(u => u.Id == id, tracked:false);
+            var hotel = await _dbHotel.GetAsync(u => u.Id == id, tracked: false);
 
             HotelUpdateDTO hotelDTO = _mapper.Map<HotelUpdateDTO>(hotel);
 
